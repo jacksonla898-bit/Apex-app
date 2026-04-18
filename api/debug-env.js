be15ddfc2f9d97f8c679bcaa0729923b63dcf632
Jackson <jacksonla898@gmail.com>
2026-04-18 12:57:23 -0400

    tmp: remove debug-env endpoint (over Hobby plan limit)

--- api/debug-env.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const key = process.env.POLYGON_API_KEY
-  res.status(200).json({
-    POLYGON_API_KEY_defined: !!key,
-    POLYGON_API_KEY_length: key ? key.length : 0,
-    ALPACA_API_KEY_defined: !!process.env.ALPACA_API_KEY,
-    ANTHROPIC_API_KEY_defined: !!process.env.ANTHROPIC_API_KEY,
-  })
-}
